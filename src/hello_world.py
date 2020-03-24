cda2713a33e88e657a797e4cd2c3d4a70d4ac3d3
John <jmaddox451@gmail.com>
2020-03-24 13:11:44 -0700

    cleanup, moved source files into src directory

---
+++ src/hello_world.py
@@ -0,0 +1,20 @@
+
+class HelloWorld ():
+    '''
+    Hello world example
+    '''
+
+    def __init__ (self, _msg):
+        self.msg = _msg
+
+
+    def get_msg (self):
+        '''
+        Return the message set in init.
+        '''
+        return self.msg
+
+
+if __name__ == "__main__":
+    hw = Hello_world('Test')
+    print(hw.get_msg())
